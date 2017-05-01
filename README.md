0cc056097420b1a658a340e36c4ddd77745ff0fe
jdmedlock <jdmedlock@gmail.com>
2017-04-30 20:31:06 -0500

    Added README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Warshall
+
+This repo contains a modified version of the Floyd-Warshall algorithm that is
+designed to identify nodes in a dependency matrix that are part of a cycle. 
+This was developed to generate sample results from test data to be included in
+this [https://medium.com/@jdmedlock/why-should-i-give-a-dag-e5f4a1f6ac7](Medium.com article).
+
--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ const warshall = require('./warshall.js');
 let deps = new warshall(3);
 
 // Test #1 - No cycle
-deps.addDependency(0,1);      // Node 0 is waiting on node 1
-deps.addDependency(2,0);      // Node 2 is waiting on node 0
+deps.addDependency(1,0);      // Node 1 is waiting on node 0
+deps.addDependency(2,1);      // Node 2 is waiting on node 1
 deps.printDependencyArray('Test #1 after adding dependencies...');
 deps.identifyDependencies();
-deps.printDependencyArray('Test #1 after identifying dependencies...');
+deps.printDependencyArray('Test #1 after identifying cycles...');
 
 // Test #2 - Create a simple dependency between two adjacent nodes
 deps.clearDependencyArray();
-deps.addDependency(0,1);      // Node 0 is waiting on node 1
-deps.addDependency(1,0);      // Node 1 is waiting on node 2
+deps.addDependency(1,0);      // Node 1 is waiting on node 0
+deps.addDependency(0,1);      // Node 1 is waiting on node 1
 deps.printDependencyArray('Test #2 after adding dependencies...');
 deps.identifyDependencies();
-deps.printDependencyArray('Test #2 after identifying dependencies...');
+deps.printDependencyArray('Test #2 after identifying cycles...');
 
 // Test #3 - Establish a transitive cycle
 deps.clearDependencyArray();
-deps.addDependency(0,1);      // Node 0 is waiting on node 1
-deps.addDependency(1,2);      // Node 1 is waiting on node 2
-deps.addDependency(2,0);      // Node 2 is waiting on node 0
+deps.addDependency(1,0);      // Node 1 is waiting on node 0
+deps.addDependency(2,1);      // Node 2 is waiting on node 1
+deps.addDependency(0,2);      // Node 0 is waiting on node 2
 deps.printDependencyArray('Test #3 after adding dependencies...');
 deps.identifyDependencies();
-deps.printDependencyArray('Test #3 after identifying dependencies...');
+deps.printDependencyArray('Test #3 after identifying cycles...');
